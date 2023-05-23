864d6dde44d30d56fd2cf5ea0bc1c718509fcbbf
AC Che <caxche@gmail.com>
2023-05-23 20:09:57 +0800

    update beian info.

--- components/templates/TheFooter.vue
+++ components/templates/TheFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="p-4 flex flex-col justify-start items-center text-xs">
-    <div>版权所有 © 2016-2021 老司机（广州）信息技术有限公司 保留所有权利 严格禁止在未经同意的情况下转载本网站之所有信息 <a href="http://www.beian.miit.gov.cn">粤ICP备19015167号-3</a></div>
+    <div>版权所有 © 2016-2021 老司机（广州）信息技术有限公司 保留所有权利 严格禁止在未经同意的情况下转载本网站之所有信息 <a href="https://beian.miit.gov.cn/">粤ICP备19015167号-3</a></div>
     <div>
-      <a target="_blank" href=" " class="flex flex-row justify-center items-center"><img src="/images/gongan.d0289dc0.png" style="float:left;"/>粤公网安备 44011302002067号</a >
+      <a target="_blank" href="https://beian.miit.gov.cn/" class="flex flex-row justify-center items-center"><img src="/images/gongan.d0289dc0.png" style="float:left;"/>粤公网安备 44011302002067号</a >
     </div>
     <div>All rights reserved.</div>
     <div><a href="mailto:cc@exdrive.cn">Contact Us</a></div>
